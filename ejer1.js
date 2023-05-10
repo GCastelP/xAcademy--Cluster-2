3080cf600a56b5b8cacc2a33867b16c698251da3
GCastelP <gcastelt@gmail.com>
2023-05-09 22:28:32 -0300

    exercise1 function & loops

---
+++ ejer1.js
@@ -0,0 +1,14 @@
+function imprimirPiramide(num) {
+    for (let i = 1; i <= num; i++) {
+      let linea = '';
+      for (let j = 1; j <= i; j++) {
+        linea += j;
+      }
+      console.log(linea);
+    }
+  }
+
+  
+  imprimirPiramide(3);
+  
+  imprimirPiramide(6);
